dfc5839630b58c1788e0f384ebc138f3149fbe9e
Cyb3rKun <cyb0rg@duck.com>
2026-05-15 18:32:08 +0200

    add emmylua types to functions in utils

--- utils/init.lua
+++ utils/init.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+---@param text string # The Text the user wants to send.
+---@param timeout number # The time in MS that the notification will last
+---@param icon nil | "info" | "warning" | "error" # 
+-- The Icon or mode of the warning
 function M.notify(text, timeout, icon)
 	hl.notification.create {
 		text = text,
