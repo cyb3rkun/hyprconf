7f9d134b2db34105e644ef03bea722b1b60b1a85
Cyb3rKun <cyb0rg@duck.com>
2026-05-15 18:29:44 +0200

    refactor monitor config to iterate over a table of monitors instead of multiple hl.monitor() calls

--- config/monitorconf.lua
+++ config/monitorconf.lua
@@ -1,7 +1,3 @@
-------------------
----- MONITORS ----
-------------------
-
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 -- For Random Monitors
 hl.monitor {
@@ -11,28 +7,32 @@ hl.monitor {
 	---@diagnostic disable-next-line: assign-type-mismatch
 	scale = 1,
 }
-hl.monitor {
-	output = "desc:Dell Inc. DELL SE2425HG 17LTB84",
-	position = "0x0",
-	mode = "1920x1080@200",
-	---@diagnostic disable-next-line: assign-type-mismatch
-	scale = 1,
-	vrr = true,
-	cm = "srgb",
+
+---@type HL.MonitorSpec[]
+local monitors = {
+	{
+		output = "desc:Dell Inc. DELL SE2425HG 17LTB84",
+		position = "0x0",
+		mode = "1920x1080@200",
+		scale = 1,
+		vrr = true,
+		cm = "srgb",
+	},
+	{
+		output = "desc:LG Electronics W2242 0x00039907",
+		mode = "1680x1050",
+		position = "auto-center-left",
+		scale = 1,
+		transform = 3,
+	},
+	{
+		output = "desc:Lenovo Group Limited 0x1144 VN-66718",
+		position = "auto-center-right",
+		mode = "1920x1200",
+		scale = 1,
+	},
 }
 
-hl.monitor {
-	output = "desc:LG Electronics W2242 0x00039907",
-	mode = "1680x1050",
-	position = "auto-center-left",
-	---@diagnostic disable-next-line: assign-type-mismatch
-	scale = 1,
-	transform = 3,
-}
-hl.monitor {
-	output = "desc:Lenovo Group Limited 0x1144 VN-66718",
-	position = "auto-center-right",
-	mode = "1920x1200",
-	---@diagnostic disable-next-line: assign-type-mismatch
-	scale = 1,
-}
+for _, m in ipairs(monitors) do
+	hl.monitor(m)
+end
