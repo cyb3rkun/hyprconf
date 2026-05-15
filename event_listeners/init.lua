3a6478f7a34634a37c01417b7384a8caa8cf7493
Cyb3rKun <cyb0rg@duck.com>
2026-05-15 18:31:28 +0200

    add todo tag

--- event_listeners/init.lua
+++ event_listeners/init.lua
@@ -5,3 +5,17 @@ hl.on("keybinds.submap", function(submap)
 	submap = submap:len() > 0 and submap or "reset"
 	utils.notify("Submap: " .. submap, 3000, "info")
 end)
+
+-- Apply static tag "luancher" to steam
+---@param win HL.Window
+hl.on("window.open", function(win)
+	if win.class:match "[Ss]team" then
+		hl.dispatch(
+			hl.dsp.window.tag { tag = "+launcher", window = win }
+		)
+	end
+end)
+
+-- TODO:
+-- Create listener to listen for games, and send noctalia ipc call to
+-- enable Noctalia performance mode when a game launches
