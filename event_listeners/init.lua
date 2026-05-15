77468820abe80f6cf3a2d4340ef5a64780fbf69b
Cyb3rKun <cyb0rg@duck.com>
2026-05-15 23:10:24 +0200

    add listeners to enable/disable noctalia performance mode

--- event_listeners/init.lua
+++ event_listeners/init.lua
@@ -9,13 +9,37 @@ end)
 -- Apply static tag "luancher" to steam
 ---@param win HL.Window
 hl.on("window.open", function(win)
-	if win.class:match "[Ss]team" then
+	if win.class:match "^[Ss]team$" then
 		hl.dispatch(
 			hl.dsp.window.tag { tag = "+launcher", window = win }
 		)
 	end
 end)
 
--- TODO:
--- Create listener to listen for games, and send noctalia ipc call to
--- enable Noctalia performance mode when a game launches
+---@param win HL.Window
+-- WARN: Relies on config.window to set the dynamic game* tag
+-- and the content_type to work.
+hl.on("window.open", function(win)
+	if win.content_type:match "game" then
+		hl.exec_cmd(
+			NoctaliaIpc .. "powerProfile enableNoctaliaPerformance"
+		)
+	end
+end)
+
+---@param win HL.Window
+-- WARN: Relies on config.window to set the dynamic game* tag
+-- and the content_type to work.
+hl.on("window.close", function(win)
+	---@type HL.Window[]
+	local games = hl.get_windows { tag = "game*" }
+
+	-- TEST: this will probably work most of the time.
+	-- might have issues with multi-window *games*
+	if #games > 1 then return end
+	if win.content_type:match "game" then
+		hl.exec_cmd(
+			NoctaliaIpc .. "powerProfile disableNoctaliaPerformance"
+		)
+	end
+end)
