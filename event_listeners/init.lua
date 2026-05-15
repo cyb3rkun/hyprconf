d188a18e96384512d20d0663ae49fa269c3615b4
Cyb3rKun <cyb0rg@duck.com>
2026-05-15 03:03:01 +0200

    revert changes to event listeners as they were mistaken

--- event_listeners/init.lua
+++ event_listeners/init.lua
@@ -1,6 +1,7 @@
-local win = require "utils.window"
+local utils = require "utils"
+
 ---@param submap string
 hl.on("keybinds.submap", function(submap)
 	submap = submap:len() > 0 and submap or "reset"
-	win.notify("Submap: " .. submap, 3000, "info")
+	utils.notify("Submap: " .. submap, 3000, "info")
 end)
