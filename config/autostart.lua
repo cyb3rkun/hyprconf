478b05e8fa5af29c71f1b785205f22b24488ca55
Cyb3rKun <cyb0rg@duck.com>
2026-05-19 15:24:01 +0200

    focus workspace 4 after launch, to prevent workspace 12 from existing

--- config/autostart.lua
+++ config/autostart.lua
@@ -8,5 +8,6 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd "qs -c noctalia-shell --no-duplicate"
 	hl.exec_cmd "discord"
 	hl.exec_cmd "nm-applet"
+	hl.dispatch(hl.dsp.focus { workspace = 4 })
 	hl.dispatch(hl.dsp.focus { workspace = 1 })
-end)
+	end)
